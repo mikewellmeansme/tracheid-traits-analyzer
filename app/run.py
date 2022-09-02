1aae44833693f724dd8425e314389281eec5c8eb
Mikhail <fff_khhh@mail.ru>
2022-09-02 14:36:03 +0700

    fix(): minor fixes

--- app/run.py
+++ app/run.py
@@ -78,11 +78,14 @@ def main(tracheid_path, climate_path, outplut_path):
         loc_df = tr_t.get_traits([trait, f'{trait}_model'])
         loc_df[f'{trait}_ind'] = loc_df[trait] / loc_df[f'{trait}_model']
         tr_t.add_trait(f'{trait}_ind', loc_df)
-        result_tables[f'{trait}_coeffs'] = pd.DataFrame(coeffs)
+        coeffs['Coef'] = ['Y_min', 'Y_as', 'a']
+        result_tables[f'coeffs_{trait}'] = pd.DataFrame(coeffs)
     
     result_tables['TracheidTraits'] = tr_t.get_traits()
     
-    for trait in ['Dmean', 'Dmax', 'CWTmax', 'CWTmean', 'Dmean_ind', 'Dmax_ind', 'CWTmax_ind', 'CWTmean_ind']:
+    traits = ['Dmean', 'Dmax', 'CWTmax', 'CWTmean', 'Dmean_ind', 'Dmax_ind', 'CWTmax_ind', 'CWTmean_ind']
+    
+    for trait in traits:
         def compare(df, index):
             r, p = dropna_pearsonr(df[index], df[trait])
             return r, p
@@ -91,10 +94,10 @@ def main(tracheid_path, climate_path, outplut_path):
         data = data[data['Year'].isin(range(1899, 2011))].reset_index(drop=True)
         fig, ax = climate_df.plot_full_daily_comparison(data, compare, trait, 21)
         df = climate_df.get_full_daily_comparison(data, compare, 21)
-        result_plots[f'dendroclim_{trait}'] = fig
-        result_tables[f'dendroclim_{trait}'] = df
+        result_plots[f'dendroclim_plot_{trait}'] = fig
+        result_tables[f'dendroclim_table_{trait}'] = df
     
-    for trait in ['Dmean', 'Dmax', 'CWTmax', 'CWTmean', 'Dmean_ind', 'Dmax_ind', 'CWTmax_ind', 'CWTmean_ind']:
+    for trait in traits:
         fig, ax = tr_t.hist(trait, subplots_kws={'sharex': 'all', 'sharey': 'all', 'figsize': (5*5, 5)})
         result_plots[f'hist_{trait}'] = fig
         fig, ax = tr_t.qqplot(trait, subplots_kws={'sharex': 'all', 'sharey': 'all', 'figsize': (5*5, 5)})
@@ -116,12 +119,17 @@ def main(tracheid_path, climate_path, outplut_path):
 
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser(description='A test program.')
+    parser = argparse.ArgumentParser(description="""
+        Generates tables and graphs describing the distribution of tracheid traits,
+        their change over time,
+        correlation with daily climate data,
+        and approximation (linear for TRW and exponential for D and CWT) with line coefficients.
+    """)
 
     parser.add_argument("tracheid_path", help="Path to csv tracheid data in Tracheids format.")
     parser.add_argument("climate_path", help="Path to csv file with daily climate data in DailyDataFrame format.")
-    parser.add_argument("outplut_path", help="Path to save obtained plots and tables.")
+    parser.add_argument("output_path", help="Path to save obtained plots and tables.")
 
     args = parser.parse_args()
 
-    main(args['tracheid_path'], args['climate_path'], args['outplut_path'])
+    main(args.tracheid_path, args.climate_path, args.output_path)
