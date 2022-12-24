c8edda5000d801029665562fbc40ed3b15f5ab25
Mikhail <46995859+mikewellmeansme@users.noreply.github.com>
2022-12-24 23:17:28 +0700

    extract plotting methods

---
+++ app/plotting/_core.py
@@ -0,0 +1,209 @@
+from __future__ import annotations
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+from matplotlib.figure import Figure, Axes
+from scipy import stats
+from sklearn.metrics import r2_score
+from typing import Dict, List, Optional, Tuple, Iterable, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.tracheid_traits import TracheidTraits
+
+from zhutils.approximators import Approximator
+
+
+def plot_tracheid_traits(
+        data: TracheidTraits,
+        trait: str,
+        *,
+        trees: Optional[List[str]] = None,
+        show_individual: bool = True,
+        show_mean: bool = True,
+        show_std: bool = True,
+        plot_kws: Optional[Dict] = None,
+        mean_kws: Optional[Dict] = None,
+        std_kws: Optional[Dict] = None,
+        axes: Optional[Axes] = None,
+        subplots_kws: Optional[Dict] = None
+) -> Tuple[Figure, Axes]:
+    
+    data.__check_trait__(trait)
+    
+    trees = trees if trees else data.__trees__
+
+    for tree in trees:
+        data.__check_tree__(tree)
+    
+    plot_kws = plot_kws or {}
+    mean_kws = mean_kws or {}
+    std_kws = std_kws or {}
+
+    data = data.__data__[data.__data__['Tree'].isin(trees)]
+    groups_per_tree = data.groupby('Tree')
+
+    fig, axes = get_subplots(1, 1, axes, subplots_kws)
+
+    if show_individual:
+        for tree in trees:
+            df = groups_per_tree.get_group(tree).reset_index()
+            axes.plot(df['Year'], df[trait], label=tree, **plot_kws)
+    
+    groups_per_year = data.groupby('Year')
+    if show_mean:
+        mean_df = groups_per_year.mean().reset_index()
+        axes.plot(mean_df['Year'], mean_df[trait], **mean_kws)
+        if show_std:
+            std_df = groups_per_year.std().reset_index()
+            axes.fill_between(std_df['Year'], mean_df[trait]-std_df[trait], mean_df[trait]+std_df[trait], **std_kws)
+    
+    axes.legend()
+    axes.set_title(trait)
+    axes.set_xlabel('Year')
+
+    return fig, axes
+
+
+def hist_tracheid_traits(
+        data : TracheidTraits,
+        trait: str,
+        axes: Optional[List[Axes]] = None,
+        subplots_kws: Optional[Dict] = None
+) -> Tuple[Figure, List[Axes]]:
+
+    data.__check_trait__(trait)
+    n = len(data.__trees__)
+    fig, axes = get_subplots(1, n, axes, subplots_kws)
+    for i, group_data in enumerate(data.__data__.groupby('Tree')):
+        tree, df = group_data
+        sns.histplot(df[trait], ax=axes[i], color='black', kde=True, stat='probability')
+        axes[i].set_title(tree)
+
+    return fig, axes
+
+
+def qqplot_tracheid_traits(
+        data: TracheidTraits,
+        trait: str,
+        dist: str = 'norm',
+        axes: Optional[List[Axes]] = None,
+        subplots_kws: Optional[Dict] = None
+) -> Tuple[Figure, List[Axes]]:
+
+    data.__check_trait__(trait)
+    n = len(data.__trees__)
+    fig, axes = get_subplots(1, n, axes, subplots_kws)
+    for i, group_data in enumerate(data.__data__.groupby('Tree')):
+        tree, df = group_data
+        stats.probplot(df[trait], dist=dist, plot=axes[i])
+        axes[i].set_title(tree)
+
+    return fig, axes
+
+
+def scatter_tracheid_traits(
+        data: TracheidTraits,
+        x_trait: str,
+        y_trait: str,
+        *,
+        trees: Optional[List[str]] = None,
+        xlabel: Optional[str] = None,
+        ylabel: Optional[str] = None,
+        approximator: Optional[Approximator] = None,
+        approximator_kws: Optional[Dict] = None,
+        plot_kws: Optional[Dict] = None,
+        scatter_kws: Optional[Dict] = None,
+        axes: Optional[List[Axes]] = None,
+        subplots_kws: Optional[Dict] = None,
+        show_r2: bool = True
+) -> Tuple[Figure, List[Axes]]:
+
+    data.__check_trait__(x_trait)
+    data.__check_trait__(y_trait)
+
+    trees = trees or sorted(data.__trees__)
+
+    for tree in trees:
+        data.__check_tree__(tree)
+
+    approximator_kws = approximator_kws or {}
+    plot_kws = plot_kws or {}
+    scatter_kws = scatter_kws or {}
+
+    n = len(trees)
+    fig, axes = get_subplots(1, n, axes, subplots_kws)
+
+    groups = data.__data__.groupby('Tree')
+
+    plot_label = plot_kws.pop('label', None)
+
+    for i, tree in enumerate(trees):
+        df = groups.get_group(tree)
+        if approximator is not None:
+            approximator.fit(df[x_trait], df[y_trait], **approximator_kws)
+            equation = approximator.get_equation(2)
+            r2 = f'\n$R^{{2}}={r2_score(df[y_trait], approximator.predict(df[x_trait])):.2f}$' if show_r2 else ''
+            x = np.arange(min(df[x_trait]), max(df[x_trait]), 1/len(df[x_trait]))
+            y = approximator.predict(x)
+            
+            label = plot_label if plot_label else equation
+
+            axes[i].plot(x, y, label=label + r2, **plot_kws)
+        
+        axes[i].scatter(df[x_trait], df[y_trait], **scatter_kws)
+        axes[i].set_title(tree)
+        axes[i].set_xlabel(xlabel if xlabel else x_trait)
+        axes[i].set_ylabel(ylabel if ylabel else y_trait)
+        axes[i].legend(frameon=False)
+
+    return fig, axes
+
+
+def plot_tracheid_traits_sample_depth(
+        data: TracheidTraits,
+        axes: Optional[Axes] = None,
+        subplots_kws: Optional[Dict] = None,
+        barplot_kws: Optional[Dict] = None
+    ) -> Tuple[Figure, Axes]:
+
+    barplot_kws = barplot_kws or {}
+
+    sample_depth = data.get_sample_depth()
+    fig, axes = get_subplots(1, 1, axes, subplots_kws)
+    axes.bar(sample_depth['Year'], sample_depth['Depth'], **barplot_kws)
+    axes.set_xlabel('Year')
+    axes.set_ylabel('Sample deth (trees)')
+
+    return fig, axes
+
+
+def get_subplots(
+        nrows: int,
+        ncols: int,
+        axes: Optional[List[Axes]] = None,
+        subplots_kws: Optional[Dict] = None
+) -> Tuple[Figure, List[Axes]]:
+    if axes is None:
+
+        subplots_kws = {
+            'sharex': 'all',
+            'sharey': 'all',
+            'figsize': (ncols * 3, nrows * 3)
+        } if subplots_kws is None else subplots_kws
+
+        fig, axes = plt.subplots(
+            ncols=ncols,
+            nrows=nrows,
+            **subplots_kws
+        )
+    else:
+        if isinstance(axes, Iterable):
+            if len(axes.shape) == 2:
+                fig = axes[0, 0].figure
+            else:
+                fig = axes[0].figure
+        else:
+            fig = axes.figure
+    return fig, axes
--- app/tracheid_traits.py
+++ app/tracheid_traits.py
@@ -1,12 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+import app.plotting
 
-from matplotlib.figure import Figure, Axes
 from pandas import DataFrame, unique, merge, concat
-from scipy import stats
-from sklearn.metrics import r2_score
-from typing import Dict, List, Optional, Tuple, Union, Iterable
+from typing import Dict, List, Optional, Tuple, Union
 from zhutils.approximators import Approximator
 from zhutils.tracheids import Tracheids
 
@@ -90,148 +85,16 @@ class TracheidTraits:
     
     def get_trees(self) -> List[str]:
         return self.__trees__.copy()
-    
-    def plot(
-            self,
-            trait: str,
-            *,
-            trees: Optional[List[str]] = None,
-            show_individual: bool = True,
-            show_mean: bool = True,
-            show_std: bool = True,
-            plot_kws: Optional[Dict] = None,
-            mean_kws: Optional[Dict] = None,
-            std_kws: Optional[Dict] = None,
-            axes: Optional[Axes] = None,
-            subplots_kws: Optional[Dict] = None
-    ) -> Tuple[Figure, Axes]:
-    
-        self.__check_trait__(trait)
-        
-        trees = trees if trees else self.__trees__
 
-        for tree in trees:
-            self.__check_tree__(tree)
-        
-        plot_kws = plot_kws or {}
-        mean_kws = mean_kws or {}
-        std_kws = std_kws or {}
+    # ----------------------------------------------------------------------
+    # Add plotting methods to TracheidTraits
+    plot = app.plotting.plot_tracheid_traits
+    hist = app.plotting.hist_tracheid_traits
+    qqplot = app.plotting.qqplot_tracheid_traits
+    scatter = app.plotting.scatter_tracheid_traits
+    plot_sample_depth = app.plotting.plot_tracheid_traits_sample_depth
 
-        data = self.__data__[self.__data__['Tree'].isin(trees)]
-        groups_per_tree = data.groupby('Tree')
-
-        fig, axes = self.__get_subplots__(1, 1, axes, subplots_kws)
-
-        if show_individual:
-            for tree in trees:
-                df = groups_per_tree.get_group(tree).reset_index()
-                axes.plot(df['Year'], df[trait], label=tree, **plot_kws)
-        
-        groups_per_year = data.groupby('Year')
-        if show_mean:
-            mean_df = groups_per_year.mean().reset_index()
-            axes.plot(mean_df['Year'], mean_df[trait], **mean_kws)
-            if show_std:
-                std_df = groups_per_year.std().reset_index()
-                axes.fill_between(std_df['Year'], mean_df[trait]-std_df[trait], mean_df[trait]+std_df[trait], **std_kws)
-        
-        axes.legend()
-        axes.set_title(trait)
-        axes.set_xlabel('Year')
-
-        return fig, axes
-
-    def hist(
-            self,
-            trait: str,
-            axes: Optional[List[Axes]] = None,
-            subplots_kws: Optional[Dict] = None
-    ) -> Tuple[Figure, List[Axes]]:
-
-        self.__check_trait__(trait)
-        n = len(self.__trees__)
-        fig, axes = self.__get_subplots__(1, n, axes, subplots_kws)
-        for i, group_data in enumerate(self.__data__.groupby('Tree')):
-            tree, df = group_data
-            sns.histplot(df[trait], ax=axes[i], color='black', kde=True, stat='probability')
-            axes[i].set_title(tree)
-
-        return fig, axes
-
-    def qqplot(
-            self,
-            trait: str,
-            dist: str = 'norm',
-            axes: Optional[List[Axes]] = None,
-            subplots_kws: Optional[Dict] = None
-    ) -> Tuple[Figure, List[Axes]]:
-
-        self.__check_trait__(trait)
-        n = len(self.__trees__)
-        fig, axes = self.__get_subplots__(1, n, axes, subplots_kws)
-        for i, group_data in enumerate(self.__data__.groupby('Tree')):
-            tree, df = group_data
-            stats.probplot(df[trait], dist=dist, plot=axes[i])
-            axes[i].set_title(tree)
-
-        return fig, axes
-
-    def scatter(
-            self,
-            x_trait: str,
-            y_trait: str,
-            *,
-            trees: Optional[List[str]] = None,
-            xlabel: Optional[str] = None,
-            ylabel: Optional[str] = None,
-            approximator: Optional[Approximator] = None,
-            approximator_kws: Optional[Dict] = None,
-            plot_kws: Optional[Dict] = None,
-            scatter_kws: Optional[Dict] = None,
-            axes: Optional[List[Axes]] = None,
-            subplots_kws: Optional[Dict] = None,
-            show_r2: bool = True
-    ) -> Tuple[Figure, List[Axes]]:
-
-        self.__check_trait__(x_trait)
-        self.__check_trait__(y_trait)
-
-        trees = trees or sorted(self.__trees__)
-
-        for tree in trees:
-            self.__check_tree__(tree)
-
-        approximator_kws = approximator_kws or {}
-        plot_kws = plot_kws or {}
-        scatter_kws = scatter_kws or {}
-
-        n = len(trees)
-        fig, axes = self.__get_subplots__(1, n, axes, subplots_kws)
-
-        groups = self.__data__.groupby('Tree')
-
-        plot_label = plot_kws.pop('label', None)
-
-        for i, tree in enumerate(trees):
-            df = groups.get_group(tree)
-            if approximator is not None:
-                approximator.fit(df[x_trait], df[y_trait], **approximator_kws)
-                equation = approximator.get_equation(2)
-                r2 = f'\n$R^{{2}}={r2_score(df[y_trait], approximator.predict(df[x_trait])):.2f}$' if show_r2 else ''
-                x = np.arange(min(df[x_trait]), max(df[x_trait]), 1/len(df[x_trait]))
-                y = approximator.predict(x)
-                
-                label = plot_label if plot_label else equation
-
-                axes[i].plot(x, y, label=label + r2, **plot_kws)
-            
-            axes[i].scatter(df[x_trait], df[y_trait], **scatter_kws)
-            axes[i].set_title(tree)
-            axes[i].set_xlabel(xlabel if xlabel else x_trait)
-            axes[i].set_ylabel(ylabel if ylabel else y_trait)
-            axes[i].legend(frameon=False)
-
-        return fig, axes
+    # ----------------------------------------------------------------------
 
     def apply_model(
             self,
@@ -270,53 +133,6 @@ class TracheidTraits:
         result = result[['Year', 'Tree']].rename(columns={'Tree': 'Depth'})
         return result
     
-    def plot_sample_depth(
-            self,
-            axes: Optional[Axes] = None,
-            subplots_kws: Optional[Dict] = None,
-            barplot_kws: Optional[Dict] = None
-        ) -> Tuple[Figure, Axes]:
-
-        barplot_kws = barplot_kws or {}
-
-        sample_depth = self.get_sample_depth()
-        fig, axes = self.__get_subplots__(1, 1, axes, subplots_kws)
-        axes.bar(sample_depth['Year'], sample_depth['Depth'], **barplot_kws)
-        axes.set_xlabel('Year')
-        axes.set_ylabel('Sample deth (trees)')
-
-        return fig, axes
-
-    @staticmethod
-    def __get_subplots__(
-            nrows: int,
-            ncols: int,
-            axes: Optional[List[Axes]] = None,
-            subplots_kws: Optional[Dict] = None
-    ) -> Tuple[Figure, List[Axes]]:
-        if axes is None:
-
-            subplots_kws = {
-                'sharex': 'all',
-                'sharey': 'all',
-                'figsize': (ncols * 3, nrows * 3)
-            } if subplots_kws is None else subplots_kws
-
-            fig, axes = plt.subplots(
-                ncols=ncols,
-                nrows=nrows,
-                **subplots_kws
-            )
-        else:
-            if isinstance(axes, Iterable):
-                if len(axes.shape) == 2:
-                    fig = axes[0, 0].figure
-                else:
-                    fig = axes[0].figure
-            else:
-                fig = axes.figure
-        return fig, axes
-
     def __check_trait__(self, trait: str) -> None:
         if trait not in self.__names__:
             raise KeyError(f'Trait name "{trait}" is not in the list of trait names!')
